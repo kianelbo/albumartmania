0201fc214f41ab725c2d0c6eaddf04574aac5267
Kian Eliasi <kian.elbo@gmail.com>
2025-10-31 11:17:27 +0330

    Add image cropper

---
+++ image.py
@@ -0,0 +1,18 @@
+import random
+from PIL import Image
+
+def crop_image(image_path, output_path="./cropped", reveal_ratio=0.25):
+    with Image.open(image_path) as img:
+        width, height = img.size
+
+        crop_width = int(width * reveal_ratio)
+        crop_height = int(height * reveal_ratio)
+
+        x_start = random.randint(0, width - crop_width)
+        y_start = random.randint(0, height - crop_height)
+
+        cropped = img.crop((x_start, y_start, x_start + crop_width, y_start + crop_height))
+        ext = image_path.split(".")[-1]
+        cropped.save(f"{output_path}.{ext}")
+
+    return output_path
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from crawler import get_random_album_cover
+from image import crop_image
 
 
 if __name__ == "__main__":
@@ -6,3 +7,5 @@ if __name__ == "__main__":
 
     print(f"{result['year']} — {result['artist']} — {result['album']}")
     print(f"Cover: {result['cover_url']}")
+
+    crop_image(result["downloaded_path"])
